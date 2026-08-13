e45e698d67022447351cc40f602456718645b9dd
Emmyblaqr <adedayoabiala@gmail.com>
2026-08-13 13:00:47 +0100

    Update asset reference from ledger.html to index.html

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 const CACHE = 'ledger-v1';
-const ASSETS = ['./', './ledger.html', './manifest.webmanifest', './icon.svg'];
+const ASSETS = ['./', './index.html', './manifest.webmanifest', './icon.svg'];
 
 self.addEventListener('install', event => {
   event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)).then(() => self.skipWaiting()));
